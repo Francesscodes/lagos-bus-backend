614714a5ba95f591b9be7a12e51085cf89583622
Ekezie Ezinne <zynefrancess6444@gmail.com>
2026-05-05 12:48:07 +0100

    fix: correct model filename casing for Linux deployment

--- models/index.js
+++ models/index.js
@@ -1,16 +1,14 @@
 // models/index.js
 const sequelize = require('../config/database');
-const User = require('./User');
-const Student = require('./Student');
-const Drivers = require('./Drivers');
-const AttendanceLog = require('./AttendanceLog');
+const User = require('./user');
+const Student = require('./student');
+const Drivers = require('./drivers');
+const AttendanceLog = require('./attendanceLog');
 
 // --- Define Associations ---
-// A User (Parent) can have many Students
 User.hasMany(Student, { foreignKey: 'parentId' });
 Student.belongsTo(User, { foreignKey: 'parentId' });
 
-// A Student can have many AttendanceLogs
 Student.hasMany(AttendanceLog, { foreignKey: 'studentId' });
 AttendanceLog.belongsTo(Student, { foreignKey: 'studentId' });
 
